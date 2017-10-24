661561efec61d11cf699e4b4dbcc139d9a472b2c
Marko <marc88cap@gmail.com>
2017-10-24 23:05:29 +0200

    Renamed and Updated Exponentiation.java

---
+++ src/compilersimulation/instructiontypes/groups/types/Exponentiation.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilersimulation.instructiontypes.groups.types;
+
+import compilersimulation.exceptions.OverflowException;
+import compilersimulation.instructiontypes.groups.Arithmetic;
+
+/**
+ *
+ * @author markoc
+ */
+public class Exponentiation extends Arithmetic{
+    
+    public Exponentiation(int code){
+	super(code);
+    }
+    
+    @Override
+    public void executeInstruction() throws Exception{
+        if(errorInput((int)(Math.pow(accumulator, super.memory.fetchAtIndex(operand)))))
+	    throw new OverflowException();
+        
+	accumulator = Math.pow(accumulator, super.memory.fetchAtIndex(operand));
+    }
+}
